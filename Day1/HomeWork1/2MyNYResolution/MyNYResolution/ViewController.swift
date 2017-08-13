3813a036e375a9a30bafd846c7a976f451b9f11c
Amnuay M <amnuay@gmail.com>
2017-08-13 17:44:15 +0700

    Added Day1 and HomeWork Done

---
+++ Day1/HomeWork1/2MyNYResolution/MyNYResolution/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyNYResolution
+//
+//  Created by DrKeng on 8/8/2560 BE.
+//  Copyright © 2560 True Incube The App Builder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Day1/HomeWork1/3MyCalculator/MyCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by DrKeng on 8/9/2560 BE.
+//  Copyright © 2560 True Incube The App Builder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
